72799337022ff6ad91fca75756ed6d4323c3d980
Sridhar-vtu24391 <vtu24391@veltech.edu.in>
2026-03-24 22:55:08 +0530

    Delete TASK 13/product directory

--- TASK 13/product/src/main/java/com/example/product/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.product.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
