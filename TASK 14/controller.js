6c5d742f7ea240d100e4d9c8eb6d90d3b645078e
Sridhar-vtu24391 <vtu24391@veltech.edu.in>
2026-04-21 23:26:27 +0530

    Add files via upload

---
+++ TASK 14/controller.js
@@ -0,0 +1,13 @@
+package com.example.identityservice.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/{id}")
+    public String getUser(@PathVariable String id) {
+        return "User Details for ID: " + id + " (from Identity Service)";
+    }
+}
